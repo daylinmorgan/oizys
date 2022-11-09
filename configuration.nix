f22503d582e4a8ed54be87e9aac71ffad34099c3
Daylin Morgan <daylinmorgan@gmail.com>
2022-11-09 05:58:31 +0000

    removal

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { lib, config, pkgs, ... }:
 
 {
   imports =
     [
       # Include the results of the hardware scan.
-      #     ./hardware-configuration.nix
       <nixpkgs/nixos/modules/installer/virtualbox-demo.nix>
     ];
 
@@ -19,37 +14,18 @@
 
   #  programs.nix-ld.enable = true;
 
-  # Use the systemd-boot EFI boot loader.
-  #  boot.loader.systemd-boot.enable = true;
-  #  boot.loader.efi.canTouchEfiVariables = true;
-
   # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
   # for compatibility add zsh to list of /etc/shells
   environment.shells = with pkgs; [ zsh ];
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # xservices 
+  # xstuffs
   services.xserver.enable = true;
   services.xserver.autorun = false;
   services.xserver.displayManager.startx.enable = true;
@@ -68,7 +44,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-
     fuse
     zsh
 
@@ -77,29 +52,30 @@
     gcc
 
     git
-    vim
 
+    vim
     neovim
+    gh
 
     nixpkgs-fmt
 
-    autorandr
     xdotool
     wezterm
     eww
     rofi
-    picom-jonaburg
+    picom
     dunst
-
-    gh
 
     firefox
     (python3.withPackages (p: with  p; [ pynvim ]))
-    python310Packages.pip
   ];
 
 
   fonts.fonts = with pkgs; [
+    font-awesome
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
 
@@ -114,38 +90,5 @@
     ];
     NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
   };
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
