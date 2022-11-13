458c064bd0f7f396215d1351299a7d5227a71ae4
Daylin Morgan <daylinmorgan@gmail.com>
2022-11-13 16:00:48 -0600

    update host name

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     {
       nixosConfigurations = {
 
-        nixos = inputs.nixpkgs.lib.nixosSystem {
+        nixos-vm = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             ./configuration.nix
