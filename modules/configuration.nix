42842153d69cce5ba5a64e433761adcf3893bd34
Daylin Morgan <daylinmorgan@gmail.com>
2023-01-17 12:32:42 -0600

    WIP

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -49,9 +49,6 @@
     (nerdfonts.override { fonts = [ "FiraCode" "FiraMono" ]; })
   ];
 
-  # for compatibility add zsh to list of /etc/shells
-  environment.shells = with pkgs; [ zsh ];
-
 
   environment.systemPackages = with pkgs; [
     fuse
@@ -107,20 +104,5 @@
     rustup
 
   ];
-
-
-  environment.etc = {
-    issue.source = ./etc/issue;
-  };
-
-  environment.variables = {
-    NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-      stdenv.cc.cc
-      openssl
-
-      zlib # for delta
-    ];
-    NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
-  };
 
 }
