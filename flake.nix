27da15469149c4966e85dcb904e7454611d3fcc6
Daylin Morgan <daylinmorgan@gmail.com>
2023-04-26 12:41:26 -0500

    unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "An example NixOS configuration";
 
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/22.11"; };
+    nixpkgs = { url = "github:nixos/nixpkgs/master"; };
   };
 
 
