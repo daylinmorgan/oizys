b9505b84bcd8f056d7b7998f3d719dc72023eb8d
Daylin Morgan <daylinmorgan@gmail.com>
2023-07-25 11:07:57 -0500

    update rune

--- utils/rune.sh
+++ utils/rune.sh
@@ -19,15 +19,14 @@ if [[ $# -eq 0 ]]; then
 fi
 
 rune=$1
-echo $2
 color=${36:-$2}
 
 # tmp this?
 FILENAME="Runic_letter_${rune}.png"
 
-wget -O $FILENAME "${IMG_SRC[$rune]}"
-printf "\033[1;%dm\n%s\033[0m \033[1m%s\033[0m\n\n" \
+[[ -f "$FILENAME" ]] || wget -O "$FILENAME" "${IMG_SRC[$rune]}"
+
+printf "\033[1;%dm\n%s\033[0m\n\n" \
 	"$color" \
-	"$(ascii-image-converter "$FILENAME" -n -H 10 -b)" \
-	"$rune" \
+	"$(ascii-image-converter "$FILENAME" -n -H 15 -b)" \
 	>"${rune}.txt"
