c87b8487ed71f0cc72553e8f45bf22e1a922af4a
Daylin Morgan <daylinmorgan@gmail.com>
2024-01-26 00:11:42 -0600

    add the cache links to nixConfig now that I actually understand it

--- flake.nix
+++ flake.nix
@@ -17,6 +17,18 @@
     wezterm.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+      "https://daylin.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "daylin.cachix.org-1:fLdSnbhKjtOVea6H9KqXeir+PyhO+sDSPhEW66ClE/k="
+    ];
+  };
   outputs = inputs @ {
     self,
     nixpkgs,
