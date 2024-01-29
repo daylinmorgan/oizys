a5c7f415e58224cb93cd09d91cf4f259b3ca7859
Daylin Morgan <daylinmorgan@gmail.com>
2024-01-29 09:27:35 -0600

    remove extra shebang

--- hosts/algiz/default.nix
+++ hosts/algiz/default.nix
@@ -4,7 +4,6 @@
   ...
 }: let
   gitea-shim = pkgs.writeShellScriptBin "gitea" ''
-  #!/bin/sh
   ssh -p 2222 -o StrictHostKeyChecking=no git@127.0.0.1 "SSH_ORIGINAL_COMMAND=\"$SSH_ORIGINAL_COMMAND\" $0 $@"
   '';
 
