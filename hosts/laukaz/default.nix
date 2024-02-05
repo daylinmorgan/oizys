9a4c1b10133ea56f88c789e656b63f73de76888a
Daylin Morgan <daylinmorgan@gmail.com>
2024-02-05 17:41:29 -0600

    WIP

--- hosts/laukaz/default.nix
+++ hosts/laukaz/default.nix
@@ -5,10 +5,10 @@
 }: {
 
   ## kodi
-  users.extraUsers.kodi.isNormalUser = true;
-  services.cage.user = "kodi";
-  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
-  services.cage.enable = true;
+  # users.extraUsers.kodi.isNormalUser = true;
+  # services.cage.user = "kodi";
+  # services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
+  # services.cage.enable = true;
   ##
 
   security.sudo.wheelNeedsPassword = false;
