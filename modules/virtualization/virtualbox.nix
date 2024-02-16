54552f1e44c0b88eae54c56814eed4ca8d33c4c4
Daylin Morgan <daylinmorgan@gmail.com>
2024-02-15 21:26:59 -0600

    i don't think the guest is equal to the "guest additions"

--- modules/virtualization/virtualbox.nix
+++ modules/virtualization/virtualbox.nix
@@ -1,7 +1,7 @@
 {...}: {
   virtualisation.virtualbox = {
     host.enable = true;
-    guest.enable = true;
+    # guest.enable = true;
   };
   users.extraGroups.vboxusers.members = ["daylin"];
 }
