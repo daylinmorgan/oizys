fa0c1cb877494dc8689ec264981cbe3e9279c3da
Daylin Morgan <daylinmorgan@gmail.com>
2024-02-19 10:54:03 -0600

    most minimal flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,23 @@
 {
   description = "nix begat oizys";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    hyprland.url = "github:hyprwm/Hyprland/main";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland-contrib.url = "github:hyprwm/contrib";
-    hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
-    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-wayland.inputs.nix-eval-jobs.follows = "nix-eval-jobs";
-    nix-eval-jobs.url = "github:nix-community/nix-eval-jobs";
-    nix-eval-jobs.inputs.nixpkgs.follows = "nixpkgs";
-    pinix.url = "github:remi-dupre/pinix";
-    pinix.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.inputs.url = "./inputs";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://hyprland.cachix.org"
-      "https://nixpkgs-wayland.cachix.org"
-      "https://daylin.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      "daylin.cachix.org-1:fLdSnbhKjtOVea6H9KqXeir+PyhO+sDSPhEW66ClE/k="
-    ];
-  };
-
-  outputs = inputs @ {nixpkgs, ...}: let
-    lib = import ./lib {inherit inputs nixpkgs;};
+  # nixConfig = {
+  #   extra-substituters = [
+  #     "https://hyprland.cachix.org"
+  #     "https://nixpkgs-wayland.cachix.org"
+  #     "https://daylin.cachix.org"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  #     "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+  #     "daylin.cachix.org-1:fLdSnbhKjtOVea6H9KqXeir+PyhO+sDSPhEW66ClE/k="
+  #   ];
+  # };
+  #
+  outputs = {inputs, ...}: let
+    lib = import ./lib {nixpkgs = inputs.inputs.nixpkgs; inputs = inputs;};
     inherit (lib) findModules buildHosts buildOizys;
   in {
     nixosModules = findModules ./modules;
---
+++ inputs/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "nix begat oizys";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    hyprland.url = "github:hyprwm/Hyprland/main";
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+
+    hyprland-contrib.url = "github:hyprwm/contrib";
+    hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-eval-jobs.follows = "nix-eval-jobs";
+    };
+
+    nix-eval-jobs.url = "github:nix-community/nix-eval-jobs";
+    nix-eval-jobs.inputs.nixpkgs.follows = "nixpkgs";
+
+    pinix.url = "github:remi-dupre/pinix";
+    pinix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+      "https://daylin.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "daylin.cachix.org-1:fLdSnbhKjtOVea6H9KqXeir+PyhO+sDSPhEW66ClE/k="
+    ];
+  };
+  outputs = _: {};
+}
