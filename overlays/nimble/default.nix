a67cd9d0edf136c5516038d68e78f366073c052a
Daylin Morgan <daylinmorgan@gmail.com>
2024-03-11 14:02:10 -0500

    use more recent nimble

--- overlays/nimble/default.nix
+++ overlays/nimble/default.nix
@@ -1,14 +1,15 @@
 {...}: (final: prev: {
   nimble = prev.nimble.overrideNimAttrs {
-    version = "0.14.2-f74bf2";
+    version = "0.14.2-5e7901760e89108476a4e21976a0ef783403e8fe";
     requiredNimVersion = 2;
     buildInputs = [prev.pkgs.openssl];
 
     src = final.fetchFromGitHub {
       owner = "nim-lang";
       repo = "nimble";
-      # more recent commit
-      rev = "f74bf2bc388f7a0154104b4bcaa093a499d3f0f7";
+
+      # most recent commit 2024-03-11
+      rev = "5e7901760e89108476a4e21976a0ef783403e8fe";
       hash = "sha256-8b5yKvEl7c7wA/8cpdaN2CSvawQJzuRce6mULj3z/mI=";
     };
   };
