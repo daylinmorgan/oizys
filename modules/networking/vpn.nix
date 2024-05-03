8f148db783bb3b4b2e0f46e956396b7996dfde7b
Daylin Morgan <me@dayl.in>
2024-05-03 12:39:20 -0500

    add expressvpn config

--- modules/networking/vpn.nix
+++ modules/networking/vpn.nix
@@ -5,5 +5,16 @@
   ...
 }:
 mkOizysModule config "vpn" {
-  environment.systemPackages = [pkgs.openconnect];
+  environment.systemPackages = with pkgs; [openconnect openvpn];
+
+  services.openvpn.servers = {
+    express-ny = {
+      config = ''
+        config /home/daylin/.config/openvpn/express-ny/config.ovpn
+        auth-user-pass /home/daylin/.config/openvpn/express-ny/credentials
+      '';
+      autoStart = false;
+      updateResolvConf = true;
+    };
+};
 }
