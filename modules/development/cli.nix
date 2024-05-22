d5c40e5bb0484181af40c412c223b2f8bd1b2317
Daylin Morgan <me@dayl.in>
2024-05-22 10:32:47 -0500

    actually suppress direnv output

--- modules/development/cli.nix
+++ modules/development/cli.nix
@@ -2,29 +2,37 @@
   inputs,
   pkgs,
   config,
+  enabled,
   mkDefaultOizysModule,
   ...
 }:
 mkDefaultOizysModule config "cli" {
-  programs.direnv.enable = true;
 
-  environment.systemPackages = (with pkgs; [
-    chezmoi
-    zoxide
-    lsd
-    fzf
-    eza
+  programs.direnv = enabled;
 
-    # utils
-    fd
-    bat
-    delta
-    ripgrep
+  environment.etc = {
+    "direnv/direnv.toml".text = ''
+      [global]
+      hide_env_diff=true
+    '';
+  };
 
-    glow
-    btop
-  ]
-    ) ++ [
-    inputs.tsm.packages.${pkgs.system}.tsm]
-  ;
+  environment.systemPackages =
+    (with pkgs; [
+      chezmoi
+      zoxide
+      lsd
+      fzf
+      eza
+
+      # utils
+      fd
+      bat
+      delta
+      ripgrep
+
+      glow
+      btop
+    ])
+    ++ [ inputs.tsm.packages.${pkgs.system}.tsm ];
 }
