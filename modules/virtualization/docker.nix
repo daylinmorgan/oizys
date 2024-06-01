8ac0e868ba69e036f56238657cfb0fea23512ebe
Daylin Morgan <me@dayl.in>
2024-06-01 01:51:55 -0500

    start win10 .desktop file

--- modules/virtualization/docker.nix
+++ modules/virtualization/docker.nix
@@ -4,7 +4,22 @@
   mkOizysModule,
   ...
 }:
+let
+# TODO: polish this up
+ win10vm = pkgs.stdenvNoCC.mkDerivation {
+  name = "win10vm";
+  unpackPhase = "true";
+  buildPhase = "mkdir $out";
+  version = "unstable";
+  desktopItem = pkgs.makeDesktopItem {
+    name = "win10vm";
+    exec = "VBoxManage startvm win10";
+    # icon = ""; # TODO: add windows icon
+    desktopName = "Windows 10 VM";
+  };
+};
+in 
 mkOizysModule config "docker" {
   virtualisation.docker.enable = true;
-  environment.systemPackages = with pkgs; [ lazydocker ];
+  environment.systemPackages = (with pkgs; [ lazydocker]) ++ [ win10vm];
 }
