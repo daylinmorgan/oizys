9e3eefee6ae0c01ed34f528725e8935b38f1ed0c
Daylin Morgan <me@dayl.in>
2024-06-03 10:30:27 -0500

    put win10vm in proper module

--- modules/virtualization/docker.nix
+++ modules/virtualization/docker.nix
@@ -4,22 +4,7 @@
   mkOizysModule,
   ...
 }:
-let
-# TODO: polish this up
- win10vm = pkgs.stdenvNoCC.mkDerivation {
-  name = "win10vm";
-  unpackPhase = "true";
-  buildPhase = "mkdir $out";
-  version = "unstable";
-  desktopItem = pkgs.makeDesktopItem {
-    name = "win10vm";
-    exec = "VBoxManage startvm win10";
-    # icon = ""; # TODO: add windows icon
-    desktopName = "Windows 10 VM";
-  };
-};
-in 
 mkOizysModule config "docker" {
   virtualisation.docker.enable = true;
-  environment.systemPackages = (with pkgs; [ lazydocker]) ++ [ win10vm];
+  environment.systemPackages = with pkgs; [ lazydocker];
 }
--- modules/virtualization/virtualbox.nix
+++ modules/virtualization/virtualbox.nix
@@ -1,7 +1,23 @@
-{ config, mkOizysModule, ... }:
+{ config, mkOizysModule, pkgs,... }:
+let
+# TODO: polish this up
+ win10vm = pkgs.stdenvNoCC.mkDerivation {
+  name = "win10vm";
+  unpackPhase = "true";
+  buildPhase = "mkdir $out";
+  version = "unstable";
+  desktopItem = pkgs.makeDesktopItem {
+    name = "win10vm";
+    exec = "VBoxManage startvm win10";
+    # icon = ""; # TODO: add windows icon
+    desktopName = "Windows 10 VM";
+  };
+};
+in
 mkOizysModule config "vbox" {
   virtualisation.virtualbox = {
     host.enable = true;
   };
   users.extraGroups.vboxusers.members = [ "daylin" ];
+  environment.systemPackages = [ win10vm ];
 }
