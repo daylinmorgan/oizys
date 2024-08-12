643d4799bc7ee3bdcc23f3931ee877cb5e508859
Daylin Morgan <me@dayl.in>
2024-08-12 13:53:46 -0500

    update nim-oriented overlays

--- overlays/nimble/default.nix
+++ overlays/nimble/default.nix
@@ -1,17 +1,15 @@
 { ... }:
 (final: prev: {
-  nimble = prev.nimble.overrideNimAttrs {
-    version = "0.14.2-5e7901760e89108476a4e21976a0ef783403e8fe";
+  nimble = prev.nimble.overrideNimAttrs rec {
+    version = "0.16.0";
     requiredNimVersion = 2;
     buildInputs = [ prev.pkgs.openssl ];
 
     src = final.fetchFromGitHub {
       owner = "nim-lang";
       repo = "nimble";
-
-      # most recent commit 2024-03-11
-      rev = "5e7901760e89108476a4e21976a0ef783403e8fe";
-      hash = "sha256-8b5yKvEl7c7wA/8cpdaN2CSvawQJzuRce6mULj3z/mI=";
+      rev = "v${version}";
+      hash = "sha256-nsQAUe+soRkWAFPYP5LftBCvQdkC1SpiIORscDsPQB4=";
     };
   };
 })
