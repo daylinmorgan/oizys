b9e5a980c8a984d3032b6246be7fc043c9598d55
Daylin Morgan <me@dayl.in>
2024-08-16 16:06:34 -0500

    remove comments

--- hosts/othalan/pkgs.nix
+++ hosts/othalan/pkgs.nix
@@ -7,8 +7,5 @@
       quarto
       cachix
       graphviz
-      # nix-du # failing to build suddenly? 
-      # https://github.com/symphorien/nix-du/issues/23
-      # maybe llvm related?
     ]);
 }
