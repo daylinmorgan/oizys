2c916149d74dbb61bf9fa6150c8e133df34cd350
Daylin Morgan <me@dayl.in>
2024-09-04 10:19:27 -0500

    remove debug echo

--- pkgs/oizys-nim/src/oizys/github.nim
+++ pkgs/oizys-nim/src/oizys/github.nim
@@ -19,14 +19,13 @@ proc postGhApi(url: string, body: JsonNode) =
     "Authorization"       : fmt"Bearer {ghToken}",
     "X-GitHub-Api-Version": "2022-11-28",
   })
-  echo $body
   let response = client.post(url, body = $body)
   info fmt"Status: {response.status}"
 
 proc createDispatch*(workflowFileName: string, `ref`: string) =
   ## https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
   var workflow =
-    if workflowFileName.endsWith(".yml") or workflowFileName.endsWith(".yaml"): workflowFileName 
+    if workflowFileName.endsWith(".yml") or workflowFileName.endsWith(".yaml"): workflowFileName
     else: workflowFileName & ".yml"
   info fmt"creating dispatch event for {workflow}"
   postGhApi(
