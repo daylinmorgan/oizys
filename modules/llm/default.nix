6d2146a4f35c9fc09f850b7f03603a31b9e01a5f
Daylin Morgan <me@dayl.in>
2024-09-11 13:02:53 -0500

    llm.withPlugins was removed

--- modules/llm/default.nix
+++ modules/llm/default.nix
@@ -10,15 +10,14 @@ let
   inherit (pkgs) python3Packages;
   # llm-ollama = python3Packages.callPackage ./llm-plugins/llm-ollama { };
   llm-claude3 = python3Packages.callPackage ./llm-plugins/llm-claude-3 { };
-  llm = (
-    pkgs.llm.withPlugins [
-      # llm-ollama
-      llm-claude3
-    ]
-  );
 in
 
 mkOizysModule config "llm" {
   # services.ollama = enabled;
-  environment.systemPackages = [ llm ];
+  environment.systemPackages = with pkgs; [
+    (python3.withPackages (ps: [
+      ps.llm
+      llm-claude3
+    ]))
+  ];
 }
