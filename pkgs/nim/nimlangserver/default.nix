2d6d8fb9a1370a7573ae4284e84dad9e3c476fc1
Daylin Morgan <me@dayl.in>
2024-09-24 11:20:17 -0500

    upgrade nimlangserver to v1.6.0

--- pkgs/nim/nimlangserver/default.nix
+++ pkgs/nim/nimlangserver/default.nix
@@ -1,15 +1,16 @@
 { fetchFromGitHub, buildNimPackage }:
-buildNimPackage{
+buildNimPackage( finalAttrs:  {
   pname = "nimlangserver";
-  version = "unstable";
+  version = "1.6.0";
   src = fetchFromGitHub {
-    owner = "daylinmorgan";
+    owner = "nim-lang";
     repo = "langserver";
-    rev = "26b333d0c8d62ba947a9ce9fbd59a7a77766872c";
-    # rev = "v${version}";
-    hash = "sha256-XFgA0yOfE34+bZxBgOdoK+5CWhxvppzl8QSQx1TTPpQ=";
+    # rev = "26b333d0c8d62ba947a9ce9fbd59a7a77766872c";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-rTlkbNuJbL9ke1FpHYVYduiYHUON6oACg20pBs0MaP4=";
   };
 
   doCheck = false;
   lockFile = ./lock.json;
 }
+)
