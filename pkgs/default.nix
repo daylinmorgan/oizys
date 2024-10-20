c3031d3cd7542f52be7e91fbc28b1a94bc9d4146
Daylin Morgan <me@dayl.in>
2024-10-20 08:32:41 -0500

    add distrobox package with shell completions

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
   nph = pkgs.callPackage ./nim/nph { }; # doesn't compile with 2.2.0 :/
   nimlangserver = pkgs.callPackage ./nim/nimlangserver { };
   nimble = pkgs.callPackage ./nim/nimble { };
+  distrobox = pkgs.callPackage ./distrobox {};
 }
