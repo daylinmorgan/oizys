e49988ab3515e3184bca304f17f935a9a46fc3b6
Daylin Morgan <me@dayl.in>
2024-11-18 11:18:32 -0600

    overlay style fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,10 +2,19 @@
 (loadOverlays inputs ./.)
 ++ [
   inputs.nim2nix.overlays.default # adds buildNimPackage
-  (final: _prev: {
+  (final: prev: {
     stable = import inputs.stable {
       system = final.system;
       config.allowUnfree = true;
+    };
+
+    # nixd + lix = problem, or am I just pulling in nix2.24 now?
+    nixt = prev.nixt.override {
+      nix = final.nixVersions.nix_2_24;
+    };
+
+    nixd = prev.nixt.override {
+      nix = final.nixVersions.nix_2_24;
     };
   })
 ]
