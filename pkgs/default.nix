c03776be01857f49cd358c99831199c5ef8a30bc
Daylin Morgan <me@dayl.in>
2024-11-18 14:56:24 -0600

    formatting

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,15 @@
 { pkgs, ... }:
-  let inherit (pkgs) python3Packages;
-    in
+let
+  inherit (pkgs) python3Packages;
+in
 {
   # nph = pkgs.callPackage ./nim/nph { }; # doesn't compile with 2.2.0 :/
   # nimlangserver = pkgs.callPackage ./nim/nimlangserver { };
   nimble = pkgs.callPackage ./nim/nimble { };
-  distrobox = pkgs.callPackage ./distrobox {};
-  llm = python3Packages.callPackage ./llm {};
-  llm-claude-3 = python3Packages.callPackage ./llm-plugins/llm-claude-3 {};
+
+  distrobox = pkgs.callPackage ./distrobox { };
+
+  llm = python3Packages.callPackage ./llm { };
+  llm-claude-3 = python3Packages.callPackage ./llm-plugins/llm-claude-3 { };
+
 }
