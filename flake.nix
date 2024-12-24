d7f3c7de3c64031297bd811c113415319c68b768
Daylin Morgan <me@dayl.in>
2024-12-24 09:28:14 -0600

    switch back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = inputs: (import ./lib inputs).oizysFlake;
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     stable.url = "github:nixos/nixpkgs/nixos-24.05";
     my-nixpkgs.url = "github:daylinmorgan/nixpkgs/nixos-unstable";
 
