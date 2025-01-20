0f374b91b2014c1d061c01505c5309280d748c88
Daylin Morgan <me@dayl.in>
2025-01-20 00:50:33 -0600

    different flake organization scheme

--- flake.nix
+++ flake.nix
@@ -25,85 +25,47 @@
       inputs.flake-utils.follows = "flake-utils";
     };
 
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    sops-nix.url = "github:Mic92/sops-nix";
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland/?submodules=1";
+    hyprland-contrib.url = "github:hyprwm/contrib";
+    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+    f1multiviewer.url = "github:daylinmorgan/f1multiviewer-flake";
+    tsm.url = "github:daylinmorgan/tsm?dir=nix";
+    hyprman.url = "git+https://git.dayl.in/daylin/hyprman.git";
+    nim2nix.url = "github:daylinmorgan/nim2nix";
+    utils.url = "git+https://git.dayl.in/daylin/utils.git";
+    pixi.url = "github:daylinmorgan/pixi-flake";
+    jj.url = "github:martinvonz/jj/v0.25.0";
 
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # master as of 2024.12.12
+    NixVirt.url = "github:AshleyYakeley/NixVirt?rev=fe3aaa86d4458e4f84348941297f7ba82e2a9f67";
 
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Follows
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixpkgs
+    f1multiviewer.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
+    hyprman.inputs.nixpkgs.follows = "nixpkgs";
+    jj.inputs.nixpkgs.follows = "nixpkgs";
+    nim2nix.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
+    NixVirt.inputs.nixpkgs.follows = "nixpkgs";
+    pixi.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    tsm.inputs.nixpkgs.follows = "nixpkgs";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
 
-    hyprland = {
-      url = "git+https://github.com/hyprwm/Hyprland/?submodules=1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland-contrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixpkgs-wayland = {
-      url = "github:nix-community/nixpkgs-wayland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    f1multiviewer = {
-      url = "github:daylinmorgan/f1multiviewer-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    tsm = {
-      url = "github:daylinmorgan/tsm?dir=nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        nim2nix.follows = "nim2nix";
-      };
-    };
-    hyprman = {
-      url = "git+https://git.dayl.in/daylin/hyprman.git";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nim2nix.follows = "nim2nix";
-    };
-    utils = {
-      url = "git+https://git.dayl.in/daylin/utils.git";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        nim2nix.follows = "nim2nix";
-      };
-    };
-    nim2nix = {
-      url = "github:daylinmorgan/nim2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pixi = {
-      url = "github:daylinmorgan/pixi-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    jj = {
-      url = "github:martinvonz/jj/v0.25.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    NixVirt = {
-      # master as of 2024.12.12
-      url = "github:AshleyYakeley/NixVirt?rev=fe3aaa86d4458e4f84348941297f7ba82e2a9f67";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nim2nix
+    hyprman.inputs.nim2nix.follows = "nim2nix";
+    tsm.inputs.nim2nix.follows = "nim2nix";
+    utils.inputs.nim2nix.follows = "nim2nix";
 
     lix-attic = {
       url = "git+https://git.lix.systems/nrabulinski/attic.git";
@@ -136,17 +98,10 @@
 
     # nil inputs
     hyprland.inputs.pre-commit-hooks.follows = "";
-
     nixos-wsl.inputs.flake-compat.follows = "";
-
-    nixpkgs-wayland.inputs = {
-      flake-compat.follows = "";
-      nix-eval-jobs.follows = "";
-    };
-
-    lix-attic.inputs = {
-      nix-github-actions.follows = "";
-    };
+    nixpkgs-wayland.inputs.flake-compat.follows = "";
+    nixpkgs-wayland.inputs.nix-eval-jobs.follows = "";
+    lix-attic.inputs.nix-github-actions.follows = "";
   };
 
   nixConfig = {
