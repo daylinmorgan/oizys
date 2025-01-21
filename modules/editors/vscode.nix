a9a4f2269b0e25998e3c77eb3d77b35e3e77e9d8
Daylin Morgan <me@dayl.in>
2025-01-21 13:46:02 -0600

    note to self

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -7,6 +7,8 @@
 let
   inherit (lib) mkIf;
 in
+# TODO: explore the ability to expand this so that some of the worthwile extensions are also included.
+# I don't think it will be simple to integrate vscode-fhs and vscode-with-extensions
 {
   config = mkIf config.oizys.desktop.enable {
     environment.systemPackages = with pkgs; [
