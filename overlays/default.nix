25cfa387a373a0bbfb0edd3efa793e9e71fee8f4
Daylin Morgan <me@dayl.in>
2025-01-30 12:10:47 -0600

    commenting these lines did nothing?

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,13 +12,13 @@
       config.allowUnfree = true;
     };
 
-    # nixd + lix = problem, or am I just pulling in nix2.24 now?
-    nixt = prev.nixt.override {
-      nix = final.nixVersions.nix_2_24;
-    };
-
-    nixd = prev.nixd.override {
-      nix = final.nixVersions.nix_2_24;
-    };
+    # # nixd + lix = problem, or am I just pulling in nix2.24 now?
+    # nixt = prev.nixt.override {
+    #   nix = final.nixVersions.nix_2_24;
+    # };
+    #
+    # nixd = prev.nixd.override {
+    #   nix = final.nixVersions.nix_2_24;
+    # };
   })
 ]
