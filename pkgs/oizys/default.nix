9f178c0de09cc99ecc0c36a7dc8e9335d58d7494
Daylin Morgan <me@dayl.in>
2025-02-17 18:54:50 -0600

    don't actually depend on nix-eval-jobs

--- pkgs/oizys/default.nix
+++ pkgs/oizys/default.nix
@@ -2,9 +2,6 @@
   lib,
   openssl,
   buildNimblePackage,
-
-  nix-eval-jobs,
-  makeWrapper,
 }:
 
 buildNimblePackage {
@@ -12,19 +9,10 @@ buildNimblePackage {
   name = "oizys";
   version = "unstable";
   src = lib.cleanSource ./.;
-  nativeBuildInputs = [ openssl makeWrapper ];
+  nativeBuildInputs = [ openssl];
   nimbleDepsHash = "sha256-bthmRlUO6IOYRiwVic0TPOvo0gsfD/49J2GzoIQqlF0=";
   meta = {
     description = "nix begat oizys";
   };
-
-  postFixup = ''
-    wrapProgram $out/bin/oizys \
-        --set PATH ${
-          lib.makeBinPath [
-            nix-eval-jobs
-          ]
-        }
-  '';
 
 }
