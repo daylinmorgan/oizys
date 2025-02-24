acbfb54491e24f1b6df688839b6cece432da636c
Daylin Morgan <me@dayl.in>
2025-02-24 13:38:56 -0600

    move versions into a standalone file

--- pkgs/llm/llm-with-plugins/default.nix
+++ pkgs/llm/llm-with-plugins/default.nix
@@ -8,31 +8,9 @@ let
   mkPlugin = name: attrs: python3Packages.callPackage (import (../. + "/llm-${name}") attrs) { };
 
   llm = python3Packages.callPackage ../llm { };
+  pluginVersions = import ../versions.nix;
 
-  pluginAttr = {
-    anthropic = {
-      version = "0.13";
-      hash = "sha256-eIppCyFu/2VKExkO88iRozC9AVDcRQaUKrNeLU89rrQ=";
-    };
-    gemini = {
-      version = "0.11";
-      hash = "sha256-xYtfIajEU1iqHvSPDLmg9lHEllcKpVYyUuNZUGNcccw=";
-    };
-    cmd = {
-      version = "0.2a0";
-      hash = "sha256-RhwQEllpee/XP1p0nrgL4m+KjSZzf61J8l1jJGlg94E=";
-    };
-    jq = {
-      version = "0.1.1";
-      hash = "sha256-Mf/tbB9+UdmSRpulqv5Wagr8wjDcRrNs2741DNQZhO4=";
-    };
-    python = {
-      version = "0.1";
-      hash = "sha256-Z991f0AGO5iaCeoG9dkFhTLtuR45PgCS9awCvOAuPPs=";
-    };
-  };
-
-  pluginPackages = pluginAttr |> mapAttrs mkPlugin |> attrValues;
+  pluginPackages = pluginVersions |> mapAttrs mkPlugin |> attrValues;
   pyWithLlm = (pkgs.python3.withPackages (_: [ llm ] ++ pluginPackages));
 
 in
---
+++ pkgs/llm/versions.nix
@@ -0,0 +1,22 @@
+{
+  anthropic = {
+    version = "0.13";
+    hash = "sha256-eIppCyFu/2VKExkO88iRozC9AVDcRQaUKrNeLU89rrQ=";
+  };
+  gemini = {
+    version = "0.11";
+    hash = "sha256-xYtfIajEU1iqHvSPDLmg9lHEllcKpVYyUuNZUGNcccw=";
+  };
+  cmd = {
+    version = "0.2a0";
+    hash = "sha256-RhwQEllpee/XP1p0nrgL4m+KjSZzf61J8l1jJGlg94E=";
+  };
+  jq = {
+    version = "0.1.1";
+    hash = "sha256-Mf/tbB9+UdmSRpulqv5Wagr8wjDcRrNs2741DNQZhO4=";
+  };
+  python = {
+    version = "0.1";
+    hash = "sha256-Z991f0AGO5iaCeoG9dkFhTLtuR45PgCS9awCvOAuPPs=";
+  };
+}
