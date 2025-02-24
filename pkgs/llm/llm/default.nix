d56c765296b3ec303124c28537b3d36e9d90c351
Daylin Morgan <me@dayl.in>
2025-02-24 15:27:50 -0600

    treefmt

--- pkgs/llm/llm/default.nix
+++ pkgs/llm/llm/default.nix
@@ -22,64 +22,64 @@
 
   pytestCheckHook,
 }:
-  buildPythonPackage rec {
-    pname = "llm";
-    version = "0.22";
-    pyproject = true;
+buildPythonPackage rec {
+  pname = "llm";
+  version = "0.22";
+  pyproject = true;
 
-    build-system = [ setuptools ];
+  build-system = [ setuptools ];
 
-    disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.8";
 
-    src = fetchFromGitHub {
-      owner = "simonw";
-      repo = "llm";
-      rev = "refs/tags/${version}";
-      hash = "sha256-l4tFBCIey5cOUvJ8IXLOjslc1zy9MnuiwFFP275S/Bg=";
-    };
+  src = fetchFromGitHub {
+    owner = "simonw";
+    repo = "llm";
+    rev = "refs/tags/${version}";
+    hash = "sha256-l4tFBCIey5cOUvJ8IXLOjslc1zy9MnuiwFFP275S/Bg=";
+  };
 
-    # patches = [ ./001-disable-install-uninstall-commands.patch ];
+  # patches = [ ./001-disable-install-uninstall-commands.patch ];
 
-    dependencies = [
-      click-default-group
-      numpy
-      openai
-      pip
-      pluggy
-      pydantic
-      python-ulid
-      pyyaml
-      setuptools # for pkg_resources
-      sqlite-migrate
-      sqlite-utils
-      puremagic
+  dependencies = [
+    click-default-group
+    numpy
+    openai
+    pip
+    pluggy
+    pydantic
+    python-ulid
+    pyyaml
+    setuptools # for pkg_resources
+    sqlite-migrate
+    sqlite-utils
+    puremagic
+  ];
+
+  nativeCheckInputs = [
+    cogapp
+    numpy
+    pytest-httpx
+    pytestCheckHook
+  ];
+
+  doCheck = true;
+
+  pytestFlagsArray = [
+    "-svv"
+    "tests/"
+  ];
+
+  pythonImportsCheck = [ "llm" ];
+
+  meta = with lib; {
+    homepage = "https://github.com/simonw/llm";
+    description = "Access large language models from the command-line";
+    changelog = "https://github.com/simonw/llm/releases/tag/${version}";
+    license = licenses.asl20;
+    mainProgram = "llm";
+    maintainers = with maintainers; [
+      aldoborrero
+      mccartykim
     ];
-
-    nativeCheckInputs = [
-      cogapp
-      numpy
-      pytest-httpx
-      pytestCheckHook
-    ];
-
-    doCheck = true;
-
-    pytestFlagsArray = [
-      "-svv"
-      "tests/"
-    ];
-
-    pythonImportsCheck = [ "llm" ];
-
-    meta = with lib; {
-      homepage = "https://github.com/simonw/llm";
-      description = "Access large language models from the command-line";
-      changelog = "https://github.com/simonw/llm/releases/tag/${version}";
-      license = licenses.asl20;
-      mainProgram = "llm";
-      maintainers = with maintainers; [
-        aldoborrero
-        mccartykim
-      ];
-    };
-  }
+  };
+}
