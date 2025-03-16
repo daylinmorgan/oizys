163dab5768e3880d4f8176e991ddd24a42bc263f
Daylin Morgan <me@dayl.in>
2025-03-15 23:05:51 -0500

    no nix-cache

--- flake.nix
+++ flake.nix
@@ -115,7 +115,7 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-cache.dayl.in"
+      "https://nix-cache.dayl.in/oizys"
     ];
     extra-trusted-public-keys = [
       "nix-cache.dayl.in-1:lj22Sov7m1snupBz/43O1fxyEfy/S7cxBpweD7iREcs"
