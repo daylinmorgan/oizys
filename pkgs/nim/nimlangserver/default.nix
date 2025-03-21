51bfe208ed48f6b7ce53114f7f59559b4cc06ac8
Daylin Morgan <me@dayl.in>
2025-03-21 13:21:44 -0500

    update nimlangserver

--- pkgs/nim/nimlangserver/default.nix
+++ pkgs/nim/nimlangserver/default.nix
@@ -1,12 +1,13 @@
 { fetchFromGitHub, buildNimPackage }:
 buildNimPackage (finalAttrs: {
   pname = "nimlangserver";
-  version = "1.8.1";
+  version = "1.10.0";
+
   src = fetchFromGitHub {
     owner = "nim-lang";
     repo = "langserver";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-j5YnTGPtt0WhRvNfpgO9tjAqZJA5Kt1FE1Mjqn0/DNY=";
+    hash = "sha256-KApIzGknWDb7UJkzii9rGOING4G8D31zUoWvMH4iw4A=";
   };
 
   doCheck = false;
