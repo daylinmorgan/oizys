48857d101cfca2e26248a6197dbb1cf7af8c0800
Daylin Morgan <me@dayl.in>
2025-08-14 12:42:54 -0400

    new cache :/

--- lib/substituters.nix
+++ lib/substituters.nix
@@ -5,6 +5,6 @@
   ];
   trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "oizys:DSw3mwVMM/Y+PXSVpkDlU5dLwlORuiJRGPkwr5INSMc="
+    "oizys:8R6ZZzI9+2ug4S6dyknAelQgOy3D5k/rCzAUu8/90BY="
   ];
 }
