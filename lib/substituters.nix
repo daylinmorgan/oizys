d6b8ef8c693b3e875fd3498aa8e562b7621d9d8a
Daylin Morgan <me@dayl.in>
2025-08-25 18:02:14 -0400

    update actual key

--- lib/substituters.nix
+++ lib/substituters.nix
@@ -6,7 +6,5 @@
   trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "oizys:8R6ZZzI9+2ug4S6dyknAelQgOy3D5k/rCzAUu8/90BY="
-    # i regenerated the cache but I think packages still exist signed by this key.
-    "oizys:DSw3mwVMM/Y+PXSVpkDlU5dLwlORuiJRGPkwr5INSMc="
   ];
 }
