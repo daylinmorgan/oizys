3a5162bd0f59e0d40dd5580dfc80b1fc27e3e0a4
Daylin Morgan <me@dayl.in>
2025-08-28 15:24:25 -0400

    also build a new comma

--- overlays/lix/default.nix
+++ overlays/lix/default.nix
@@ -1,11 +1,18 @@
 inputs: final: prev: {
+
+  comma = prev.comma.override {
+    nix = final.lixPackageSets.stable.lix;
+  };
+
   inherit (final.lixPackageSets.stable)
     nixpkgs-review
     nix-eval-jobs
-  # nix-direnv... I think using programs.nix-direnv already overrides the nix version I think
+    nix-index
 
-  # I don't actually use these..
-  # nix-fast-build
-  # colmena
+    # nix-direnv... I think using programs.nix-direnv already overrides the nix version I think
+
+    # I don't actually use these..
+    # nix-fast-build
+    # colmena
     ;
 }
