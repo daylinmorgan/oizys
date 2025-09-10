4aa013586f3e62e05fbe60ecea3f21b058256aa6
Daylin Morgan <me@dayl.in>
2025-09-10 15:04:26 -0400

    hide_env_diff

--- modules/development/cli.nix
+++ modules/development/cli.nix
@@ -8,16 +8,9 @@
 }:
 mkDefaultOizysModule config "cli" {
 
-  programs.direnv = enabled;
-
-  # I think I was overwriting what is generated with programs.direnv...
-  # could these be added by using programs.direnv.settings?
-  # environment.etc = {
-  #   "direnv/direnv.toml".text = ''
-  #     [global]
-  #     hide_env_diff=true
-  #   '';
-  # };
+  programs.direnv = enabled // {
+    settings.global.hide_env_diff = true;
+  };
 
   environment.systemPackages =
     (with pkgs; [
