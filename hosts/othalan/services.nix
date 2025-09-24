4c96760f8c5f3ebaa302de3c8e01cc8d537e2b93
Daylin Morgan <me@dayl.in>
2025-09-24 08:50:47 -0400

    cleanup othalan/services

--- hosts/othalan/services.nix
+++ hosts/othalan/services.nix
@@ -1,39 +1,17 @@
 {
   config,
-  #  pkgs,
   ...
 }:
-# let
-#   notes-git = ''${pkgs.git}/bin/git -C /home/daylin/stuff/notes'';
-# in
 {
   services.restic.backups.gdrive = {
     user = "daylin";
     repository = "rclone:g:archives/othalan";
     passwordFile = config.sops.secrets.restic-othalan.path;
-    paths = [
-      "/home/daylin/stuff/"
-      "/home/daylin/dev/"
-    ];
+    paths =
+      [
+        "stuff"
+        "dev"
+      ]
+      |> map (p: "/home/daylin/${p}");
   };
-
-  # systemd.services.notes-bot = {
-  #   description = "auto commit changes to notes";
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     User = "daylin";
-  #     ExecStart = ''
-  #       ${pkgs.bash}/bin/bash -c '${notes-git} commit -m ":memo: :robot: $(${pkgs.coreutils}/bin/date +\'%%T\')" --no-gpg-sign -- notes'
-  #     '';
-  #   };
-  # };
-  # systemd.timers.notes-bot-timer = {
-  #   description = "run notes commit service";
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     OnCalendar = "daily";
-  #     Persistent = true;
-  #     Unit = "notes-bot.service";
-  #   };
-  # };
 }
