1a8b9906f8c24a82dd40f98509195297f058cbe3
Daylin Morgan <me@dayl.in>
2025-10-08 12:57:13 -0400

    cleanup: remove old packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,19 +9,9 @@ let
 in
 {
   distrobox = pkgs.callPackage ./distrobox { };
-
   nimlangserver = pkgs.callPackage ./nim/nimlangserver { };
   procs = pkgs.callPackage ./nim/procs { };
   nimble = pkgs.callPackage ./nim/nimble { };
-
-  # from lix overlay
-  nix-eval-jobs = pkgs.nix-eval-jobs;
-
-  # roc = (flake.pkgs "roc").cli;
-  # roc-lang-server = (flake.pkgs "roc").lang-server;
-
-  # attic-client = (flake.pkgs "lix-attic").attic-client;
-  # attic-server = (flake.pkgs "lix-attic").attic-server;
 }
 // (flake.toPackageAttrs [
   "multiviewer"
