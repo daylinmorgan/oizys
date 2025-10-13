496002bb562d85fd5e8035bfb20355f276c67f47
Daylin Morgan <me@dayl.in>
2025-10-13 13:18:24 -0400

    add lix and friends to pkgs for convience

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,7 +12,8 @@ in
   nimlangserver = pkgs.callPackage ./nim/nimlangserver { };
   procs = pkgs.callPackage ./nim/procs { };
   nimble = pkgs.callPackage ./nim/nimble { };
-  nix-eval-jobs = (flake.pkgs "lix-module").nix-eval-jobs;
+  lix = flake.pkg "lix-module";
+  inherit (flake.pkgs "lix-module") nix-eval-jobs;
 }
 // (flake.toPackageAttrs [
   "multiviewer"
