68eed2f55098cc4585778d99357ceb16e2b98293
Daylin Morgan <me@dayl.in>
2025-10-14 10:01:03 -0400

    --use-remote-sudo -> --sudo

--- pkgs/oizys/src/oizys/nix.nim
+++ pkgs/oizys/src/oizys/nix.nim
@@ -92,7 +92,7 @@ proc newRebuildCommand(subcmd: NixosRebuildSubcmd, args: openArray[string], remo
     let host = getHosts()[0]
     if host == currentHost:
       fatalQuit "did you mean to specify a remote host?"
-    result.addArgs "--target-host", host, "--use-remote-sudo"
+    result.addArgs "--target-host", host, "--sudo"
   result.addArgs args
 
 
