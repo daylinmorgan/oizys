169cba12adcc7994d1dd93b41e17c869f5535d02
Daylin Morgan <me@dayl.in>
2025-10-27 12:29:38 -0400

    use patched atlas

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,13 +8,15 @@ let
   flake = flakeFromSystem pkgs.system;
 in
 {
+  inherit (flake.pkgs "lix-module") nix-eval-jobs;
+  lix = flake.pkg "lix-module";
+
   distrobox = pkgs.callPackage ./distrobox { };
   nimlangserver = pkgs.callPackage ./nim/nimlangserver { };
   procs = pkgs.callPackage ./nim/procs { };
   nimble = pkgs.callPackage ./nim/nimble { };
-  nim-atlas = pkgs.callPackage ./nim/atlas {};
-  lix = flake.pkg "lix-module";
-  inherit (flake.pkgs "lix-module") nix-eval-jobs;
+  # nim-atlas = pkgs.callPackage ./nim/atlas {};
+  nim-atlas = (flake.pkgs "nim2nix").atlas;
 }
 // (flake.toPackageAttrs [
   "multiviewer"
