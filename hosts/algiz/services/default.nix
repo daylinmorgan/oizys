9714fb235e148ed47724e3197c286ce6dbc491e0
Daylin Morgan <me@dayl.in>
2025-11-19 10:22:49 -0600

    multiply the bans

--- hosts/algiz/services/default.nix
+++ hosts/algiz/services/default.nix
@@ -22,6 +22,11 @@
     fail2ban = enabled // {
       maxretry = 5;
       bantime = "24h";
+      bantime-increment = enabled // {
+        multipliers = "1 2 4 8 16 32 64";
+        maxtime = "168h"; # Do not ban for more than 1 week
+        overalljails = true; # Calculate the bantime based on all the violations
+      };
     };
 
     openssh = enabled // {
