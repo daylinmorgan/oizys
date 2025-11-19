7d77cba7a100ff86bc1523e5dac621dcbf286b0b
Daylin Morgan <me@dayl.in>
2025-11-19 10:22:49 -0600

    use var lib for gotosocial

--- hosts/algiz/services/gotosocial/README.md
+++ hosts/algiz/services/gotosocial/README.md
@@ -1,5 +1,4 @@
 # gotosocial
 
-Note custom font Recursive-1.085 is placed at `/opt/gotosocial/fonts/ArrowType-Recursive-1.085`.
+Note custom font Recursive-1.085 is placed at `/var/lib/gotosocial/fonts/ArrowType-Recursive-1.085`.
 I could populate this using `nix` if I wanted.
-
--- hosts/algiz/services/gotosocial/default.nix
+++ hosts/algiz/services/gotosocial/default.nix
@@ -7,8 +7,8 @@
 
       [Container]
       Image=docker.io/superseriousbusiness/gotosocial:0.19.1
-      Volume=/opt/gotosocial/fonts:/gotosocial/web/assets/myfonts/:Z,U
-      Volume=/opt/gotosocial/data:/gotosocial/storage:Z,U
+      Volume=/var/lib/gotosocial/fonts:/gotosocial/web/assets/myfonts/:Z,U
+      Volume=/var/lib/gotosocial/data:/gotosocial/storage:Z,U
       PublishPort=3758:3758
       EnvironmentFile=${./env}
 
