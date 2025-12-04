40fba0144de1f45655fdcdf0a035c2adcb9d35ed
Daylin Morgan <me@dayl.in>
2025-12-04 15:52:25 -0500

    use "fennecui"

--- home/private_dot_mozilla/private_firefox/profile.daylin/user.js
+++ home/private_dot_mozilla/private_firefox/profile.daylin/user.js
@@ -6,16 +6,10 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // fill svg color
-user_pref("svg.context-properties.content.enabled", true);
+// user_pref("svg.context-properties.content.enabled", true);
 
 // enable :has selector
-user_pref("layout.css.has-selector.enabled", true);
-
-// integrated calculator at urlbar
-user_pref("browser.urlbar.suggest.calculator", true);
-
-// integrated unit convertor at urlbar
-user_pref("browser.urlbar.unitConversion.enabled", true);
+// user_pref("layout.css.has-selector.enabled", true);
 
 // trim url
 user_pref("browser.urlbar.trimHttps", true);
@@ -33,19 +27,11 @@ user_pref("browser.compactmode.show", true);
 // fix sidebar tab drag on linux
 user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
 
-user_pref("browser.tabs.allow_transparent_browser", true);
-
 // uidensity -> compact
 user_pref("browser.uidensity", 1);
 
-// macos transparent
-user_pref("widget.macos.titlebar-blend-mode.behind-window", true);
-
 // don't warn on about:config open
 user_pref("browser.aboutConfig.showWarning", false);
-
-user_pref("sidebar.revamp", false);
-user_pref("sidebar.verticalTabs", false);
 
 // https://rubenerd.com/mozillas-latest-quagmire/
 user_pref("browser.ml.enable", false);
@@ -60,3 +46,9 @@ user_pref("browser.tabs.groups.smart.userEnabled", false);
 user_pref("pdfjs.enableAltTextModelDownload", false);
 user_pref("pdfjs.enableGuessAltText", false);
 
+
+// https://github.com/SakuraMeadows/Sakuras-Simple-Sidebar
+user_pref("sidebar.revamp", false):
+user_pref("sidebar.old-sidebar.has-used", true);
+//user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
