c88660348c5888930fa563ec73a8f00e9ee991f3
Daylin Morgan <me@dayl.in>
2026-02-10 09:52:26 -0500

    features be deprecating out here

--- modules/nix.nix
+++ modules/nix.nix
@@ -25,6 +25,11 @@ in
         "flakes"
         "pipe-operator"
       ];
+      # lix is getting strict...surely someone else will deal with these things in nixos/nixpkgs
+      deprecated-features = [
+        "or-as-identifier"
+        "broken-string-escape"
+      ];
       use-xdg-base-directories = true;
       trusted-users = [ "@wheel" ];
       accept-flake-config = false;
