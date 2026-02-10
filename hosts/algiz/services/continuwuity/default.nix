f55cd4716feee88fb0bb641fa89d3515ba71a229
Daylin Morgan <me@dayl.in>
2026-02-10 08:42:30 -0600

    update continuwuity

--- hosts/algiz/services/continuwuity/default.nix
+++ hosts/algiz/services/continuwuity/default.nix
@@ -28,7 +28,7 @@
     Description=continuwuity
 
     [Container]
-    Image=forgejo.ellis.link/continuwuation/continuwuity:v0.5.3
+    Image=forgejo.ellis.link/continuwuation/continuwuity:v0.5.4
     Volume=/var/lib/continuwuity/data/:/var/lib/continuwuity:Z,U
     EnvironmentFile=${./env}
     EnvironmentFile=${config.sops.secrets.continuwuity-env.path}
