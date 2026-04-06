7419f7b14df58ca6488b61bf65c9a8a81eb65dcb
Daylin Morgan <me@dayl.in>
2026-04-06 11:52:32 -0500

    update soft-server

--- hosts/algiz/services/images.nix
+++ hosts/algiz/services/images.nix
@@ -5,7 +5,7 @@
   linkding = "sissbruecker/linkding:1.44.1-plus-alpine";
   otterwiki = "docker.io/redimp/otterwiki:2-slim";
   pds = "ghcr.io/bluesky-social/pds:0.4.193";
-  soft-serve = "docker.io/charmcli/soft-serve:v0.11.1";
+  soft-serve = "docker.io/charmcli/soft-serve:v0.11.6";
   yamtrack = "ghcr.io/fuzzygrim/yamtrack:0.25.0";
   yamtrack-redis = "docker.io/library/redis:8-alpine";
 }
