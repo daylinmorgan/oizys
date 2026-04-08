0ca93d8214663f3fc575657bf4227e04e39ea2b9
Daylin Morgan <me@dayl.in>
2026-04-08 09:35:19 -0500

    set algiz timezone

--- hosts/algiz/system.nix
+++ hosts/algiz/system.nix
@@ -5,6 +5,8 @@
 {
   oizys.server = enabled;
 
+  time.timeZone = "America/New_York";
+
   sops = {
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     defaultSopsFile = ./secrets.yaml;
