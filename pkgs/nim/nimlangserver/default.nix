79d30c7de2a2cd6850bb0c990eca99b0ea0e2f72
Daylin Morgan <me@dayl.in>
2026-04-08 13:28:25 -0400

    update nimlangserver

--- pkgs/nim/nimlangserver/default.nix
+++ pkgs/nim/nimlangserver/default.nix
@@ -1,19 +1,19 @@
 { fetchFromGitHub, buildNimPackage }:
 buildNimPackage (_finalAttrs: {
   pname = "nimlangserver";
-  version = "1.10.2-unstable";
+  version = "1.12.0-unstable";
 
   src = fetchFromGitHub {
     owner = "nim-lang";
     repo = "langserver";
     # rev = "v${finalAttrs.version}";
-    rev = "380e4634f2891a926e40c334161931676b074b5a";
-    hash = "sha256-Tx/2aA5q2K44E2tSlbZNwubJDHV9V+8EDFwR5c0Gjn8=";
+    rev = "aa4be4cbae932d3fbb5c5b3b28e70c2df6e7f426";
+    hash = "sha256-py7XSZRDh10dv/S9rtyP71XauyTZC268afZ+XKQfWlo=";
   };
 
   doCheck = false;
 
   # nix build '.#nimlangserver.src'
-  # nix run "github:daylinmorgan/nnl" -- result/nimble.lock -o:pkgs/nim/nimlangserver/lock.json --prefetch-git:bearssl,zlib
+  # nix run "github:daylinmorgan/nnl" -- result/nimble.lock -o:pkgs/nim/nimlangserver/lock.json --git,=bearssl,zlib
   lockFile = ./lock.json;
 })
