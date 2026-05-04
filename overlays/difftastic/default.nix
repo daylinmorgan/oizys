ef79250bcee23c0964d2aebfd7c8ef5f2dac23fa
Daylin Morgan <me@dayl.in>
2026-05-04 10:13:03 -0400

    update difftastic

--- overlays/difftastic/default.nix
+++ overlays/difftastic/default.nix
@@ -1,12 +1,12 @@
 inputs: final: prev: {
   difftastic = prev.difftastic.overrideAttrs (
     finalAttrs: _: {
-      version = "0.69.0"; # 0.68.0-nim, really
+      version = "0.69.0"; # 0.69.0-nim, really
       src = final.fetchFromGitHub {
         owner = "daylinmorgan";
         repo = "difftastic";
-        rev = "0.68.0-nim"; # finalAttrs.version;
-        hash = "sha256-v5VE8Hl1FS6ynOrmqUjezII9CB8acKDyGNDfOh1E06Q=";
+        rev = "0.69.0-nim"; # finalAttrs.version;
+        hash = "sha256-H7hmteydc7zlPjTXfH5mqEYZEfC0W4S7+unOi0a4kz8=";
       };
       cargoDeps = final.rustPlatform.importCargoLock {
         lockFile = "${finalAttrs.src}/Cargo.lock";
