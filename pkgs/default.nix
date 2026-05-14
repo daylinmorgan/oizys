2fa74877107956af7c79be9bda9e86b30bc3ca10
Daylin Morgan <me@dayl.in>
2026-05-14 13:56:58 -0400

    DRY

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,18 +8,17 @@ let
   inherit (lib) flakeFromSystem;
   flake = flakeFromSystem system;
   nim-nnl-update-script = pkgs.callPackage ./nim/nim-nnl-update-script { };
+  callNimPackage = p: pkgs.callPackage p { inherit nim-nnl-update-script; };
 in
 {
   oizys = pkgs.callPackage ../pkgs/oizys {
     inherit (lib.data.substituters) substituters trusted-public-keys;
   };
 
-  inherit nim-nnl-update-script;
-  distrobox = pkgs.callPackage ./distrobox { };
-  nimlangserver = pkgs.callPackage ./nim/nimlangserver { inherit nim-nnl-update-script; };
-  procs = pkgs.callPackage ./nim/procs { inherit nim-nnl-update-script; };
-  nimble = pkgs.callPackage ./nim/nimble { };
-  nim-atlas = pkgs.callPackage ./nim/atlas { inherit nim-nnl-update-script; };
+  nimlangserver = callNimPackage ./nim/nimlangserver;
+  procs = callNimPackage ./nim/procs;
+  nimble = callNimPackage ./nim/nimble;
+  nim-atlas = callNimPackage ./nim/atlas;
   caddy-with-plugins = pkgs.callPackage ./caddy-with-plugins { };
   firefox = pkgs.callPackage ./firefox { };
   inherit (pkgs) difftastic;
