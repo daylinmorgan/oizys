c88636bed6ea730d98f2c577c696065f1d2f42eb
Daylin Morgan <me@dayl.in>
2026-06-16 10:12:47 -0400

    bump image versions

--- hosts/algiz/services/images.nix
+++ hosts/algiz/services/images.nix
@@ -1,11 +1,11 @@
 {
   continuwuity = "forgejo.ellis.link/continuwuation/continuwuity:v0.5.9";
   forgejo = "codeberg.org/forgejo/forgejo:15.0.0";
-  gotosocial = "docker.io/superseriousbusiness/gotosocial:0.21.1";
-  linkding = "sissbruecker/linkding:1.44.1-plus-alpine";
-  otterwiki = "docker.io/redimp/otterwiki:2-slim";
-  pds = "ghcr.io/bluesky-social/pds:0.4.219";
+  gotosocial = "docker.io/superseriousbusiness/gotosocial:0.21.2";
+  # linkding = "sissbruecker/linkding:1.44.1-plus-alpine";
+  otterwiki = "docker.io/redimp/otterwiki:2.20.6-slim";
+  pds = "ghcr.io/bluesky-social/pds:0.4.5001";
   soft-serve = "docker.io/charmcli/soft-serve:v0.11.6";
-  yamtrack = "ghcr.io/fuzzygrim/yamtrack:0.25.0";
+  yamtrack = "ghcr.io/fuzzygrim/yamtrack:0.25.3";
   yamtrack-redis = "docker.io/library/redis:8-alpine";
 }
