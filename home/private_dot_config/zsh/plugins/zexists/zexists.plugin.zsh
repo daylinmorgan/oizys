11f2520461949d7c32b03d578b1e2fadd4ee05ed
Daylin Morgan <me@dayl.in>
2026-06-18 14:03:43 -0400

    various zexists plugin changes

--- home/private_dot_config/zsh/plugins/zexists/zexists.plugin.zsh
+++ home/private_dot_config/zsh/plugins/zexists/zexists.plugin.zsh
@@ -1,99 +1,95 @@
 #!/usr/bin/env zsh
 
 function source-zshcmdd {
-  setopt extended_glob
+  # Use local_options to prevent extended_glob from polluting the user's global shell state.
+  setopt local_options extended_glob
 
-  # glob search for the zshcmd.d dir
   local -a zshcmdd=()
-  [[ -n "$ZSHCMDD" ]] && zshcmdd+=($ZSHCMDD(N))
-  [[ -n "$ZDOTDIR" ]] && zshcmdd+=($ZDOTDIR/exists.d/cmd(N))
-  zshcmdd+=(${ZDOTDIR:-$HOME}/zexists.d/cmd(N))
+  
+  # Build the array of target directories. 
+  # (N) ensures the glob evaluates to an empty array instead of throwing an error if missing.
+  [[ -n "$ZSHCMDD" ]] && zshcmdd+=("$ZSHCMDD"(N))
+  [[ -n "$ZDOTDIR" ]] && zshcmdd+=("$ZDOTDIR/exists.d/cmd"(N))
+  zshcmdd+=("${ZDOTDIR:-$HOME}/zexists.d/cmd"(N))
 
   if ! (( $#zshcmdd )); then
-    echo >&2 "zexists: dir not found '${ZSHCMDD:-${ZDOTDIR:-$HOME}/zexists.d/cmd}'"
+    print -u2 "zexists: dir not found '${ZSHCMDD:-${ZDOTDIR:-$HOME}/zexists.d/cmd}'"
     return 1
   fi
+
   local -a conf_files=("$zshcmdd[1]"/*.{sh,zsh}(N))
-  local rcfile
-  local antircfile
-  # sort and source conf files
+  local rcfile antircfile
+
+  # Loop through files, sorted in ascending alphabetical order via (o).
   for rcfile in ${(o)conf_files}; do
-    # ignore files that begin with a tilde and antircfiles
     case ${rcfile:t} in 'anti'* | '~'*) continue;; esac
-    # source files only if exe with that name exists
+    
+    # Evaluate if the command matching the filename (sans extension) exists in $PATH.
     if (( $+commands[${rcfile:t:r}] )); then
+      # Synchronously source the file. This blocks prompt rendering.
       source "$rcfile"
     else
-      # if it doesn't exist try the anti version
-      antircfile=${rcfile:h}/anti-${rcfile:t}
-      [ -f $antircfile ] && source $antircfile
+      # Fallback: Look for a corresponding 'anti-' file.
+      antircfile="${rcfile:h}/anti-${rcfile:t}"
+      [[ -f "$antircfile" ]] && source "$antircfile"
     fi
   done
 }
 
 function zshdir-decode {
-  # for now just remove the 'dir' part
-  echo ${${1//-SLASH-//}/-DOT-/\.}
+  # Replaces '-SLASH-' with '/' and '-DOT-' with '.'
+  echo "${${1//-SLASH-//}/-DOT-/\.}"
 }
 
 function zshdir-encode {
-  # dir is hardcoded...
-  echo dir${${1//${HOME}\//}//\//-SLASH-}
+  # Hardcodes 'dir' prefix, removes $HOME prefix, and replaces '/' with '-SLASH-'
+  echo "dir${${1//${HOME}\//}//\//-SLASH-}"
 }
 
 function source-zshpathd {
-  setopt extended_glob
+  setopt local_options extended_glob
 
-  # glob search for the zshcmd.d dir
   local -a zshpathd=()
-  [[ -n "$ZSHPATHD" ]] && zshpathd+=($ZSHPATH(N))
-  [[ -n "$ZDOTDIR" ]] && zshpathd+=(
-    $ZDOTDIR/zexists/path(N)
-  )
-  zshpathd+=(${ZDOTDIR:-$HOME}/zexists.d/path(N))
+  
+  # Evaluate the custom variable $ZSHPATHD.
+  [[ -n "$ZSHPATHD" ]] && zshpathd+=("$ZSHPATHD"(N))
+  [[ -n "$ZDOTDIR" ]] && zshpathd+=("$ZDOTDIR/zexists/path"(N))
+  zshpathd+=("${ZDOTDIR:-$HOME}/zexists.d/path"(N))
 
   if ! (( $#zshpathd )); then
-    echo >&2 "zexists: dir not found '${ZSHPATHD:-${ZDOTDIR:-$HOME}/zexists.d/path}'"
+    print -u2 "zexists: dir not found '${ZSHPATHD:-${ZDOTDIR:-$HOME}/zexists.d/path}'"
     return 1
   fi
 
   local -a conf_files=("$zshpathd[1]"/*(N))
-  local rcfile
-  local name
-  local pathtype
-  local directory
-  local antircfile  # sort and source conf files
+  local rcfile name pathtype directory antircfile
 
   for rcfile in ${(o)conf_files}; do
-
-    if [[ ${rcfile:t} == "dir"* ]];then
+    if [[ ${rcfile:t} == dir-* ]]; then
       pathtype='dir'
-    elif [[ ${rcfile:t} == "file"* ]];then
+    elif [[ ${rcfile:t} == file-* ]]; then
       pathtype='file'
-      echo >&2 "zexists: file type not yet supported"
+      # Skip unsupported files cleanly.
+      print -u2 "zexists: file type not yet supported (${rcfile:t})"
+      continue
     else
-      echo >&2 "zexists: unknown path type for ${rcfile}"
-      return 1
+      print -u2 "zexists: unknown path type for ${rcfile:t}"
+      continue
     fi
 
-    # ignore files that begin with a tilde
     case ${rcfile:t} in '~'*) continue;; esac
-    # remove 'dir' from name
-    name=${${rcfile:t}/${pathtype}-/}
-    directory=$HOME/$(zshdir-decode ${name})
-    # source files only if exe with that name exists
-    if [[ -d $directory ]]; then
-      # echo "$directory"
-      source $rcfile
+
+    name="${${rcfile:t}/${pathtype}-/}"
+    directory="$HOME/$(zshdir-decode "$name")"
+    
+    if [[ -d "$directory" ]]; then
+      # Synchronously source the file.
+      source "$rcfile"
     else
-      # TODO: antirc naming broken
-      # # if it doesn't exist try the anti version
-      # antircfile="${rcfile:h}/anti-${pathtype}-${rcfile:t}"
-      # [[ -f $antircfile ]] && source $antircfile
+      antircfile="${rcfile:h}/anti-${rcfile:t}"
+      [[ -f "$antircfile" ]] && source "$antircfile"
     fi
-
   done
-
 }
 
 source-zshcmdd
